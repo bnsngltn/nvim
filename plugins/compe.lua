e05ee776e8a5cd6d582877319285d21730b83f61
bnsngltn <ben.sanglitan.codes@gmail.com>
2021-11-08 20:37:09 +0800

    feat(nvim cmp): rebinded autocomplete results

--- plugins/compe.lua
+++ plugins/compe.lua
@@ -8,8 +8,6 @@ local cmp = require'cmp'
       end,
     },
     mapping = {
-      ['<TAB>']= cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-      ['<S-TAB>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }), 
       ['<C-d>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
