c9630502dfee87825bd6573788b35fe4a1224679
bnsngltn <ben.sanglitan.codes@gmail.com>
2021-12-28 08:28:26 +0800

    feat(arduino stuff): add more settings

--- plugins/arduinols.lua
+++ plugins/arduinols.lua
@@ -1,6 +1,8 @@
 require'lspconfig'.arduino_language_server.setup({
   cmd = {
     "arduino-language-server",
-    "cli-config", "/home/ben/.arduino15/arduino-cli.yaml",
+    "-cli-config", "/home/ben/.arduino15/arduino-cli.yaml",
+    "-clangd", "/usr/bin/clangd",
+    "-cli", "/usr/bin/arduino-cli",
   }
 })
